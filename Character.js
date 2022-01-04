311b82e36985f75a0076a77532f74db61d2b2bfb
lucasd <derhore.lucas@gmail.com>
2022-01-04 15:16:48 +0100

    second commit fix status

--- Character.js
+++ Character.js
@@ -1,7 +1,9 @@
 class Character {
-  constructor(hp, dmg, mana) {
+  constructor(hp, dmg, mana, status = "playing", hasplayed = false) {
     this.hp = hp;
     this.dmg = dmg;
     this.mana = mana;
+    this.status = status;
+    this.hasplayed = hasplayed;
   }
 }
